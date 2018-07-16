33ea4fad91f7c1ff3b4b5e7c501a45b9843cd086
johnnkp <22496821+johnnkp@users.noreply.github.com>
2018-07-16 11:14:52 +0800

    Update ServiceSC.cs to v1.0.2.0
    https://github.com/Nummer/Destroy-Windows-10-Spying/commit/80d236b8524ac3d3292c3a77f0fe5f24e8e253db

--- DWS/lib/ServiceSC.cs
+++ DWS/lib/ServiceSC.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DWS.lib
@@ -10,15 +11,21 @@ namespace DWS.lib
     {
         public static void DisableService(string scName)
         {
-            WindowsUtil.RunCmd($"/c net stop {scName}");
-            WindowsUtil.ProcStartargs("powershell", $"-command \"Set-Service -Name {scName} -StartupType Disabled\"");
-            Logger.Log($"Disable {scName} service");
+            new Thread(o =>
+            {
+                WindowsUtil.RunCmd($"/c net stop {scName}");
+                WindowsUtil.ProcStartargs("powershell", $"-command \"Set-Service -Name {scName} -StartupType Disabled\"");
+                Logger.Log($"Disable {scName} service");
+            }).Start();
         }
 
         public static void DeleteService(string scName)
         {
-            WindowsUtil.RunCmd($"/c sc delete {scName}");
-            Logger.Log($"Delete {scName} service");
+            new Thread(o =>
+            {
+                WindowsUtil.RunCmd($"/c sc delete {scName}");
+                Logger.Log($"Delete {scName} service");
+            }).Start();
         }
     }
 }
